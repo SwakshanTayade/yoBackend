ff11637c28cfc1d8f06652744e0486dbce5731d5
swakshantayade <swakshan01022003@gmail.com>
2024-10-09 15:04:59 +0530

    added vercel and cors

--- server/index.js
+++ server/index.js
@@ -5,11 +5,28 @@ import { connectDB } from "./db.js";
 import route from "./routes/route.js";
 const app = express();
 app.use(express.json());
-app.use(cors());
 dotenv.config();
 
 
 connectDB();
+//cors configuration
+const prodOrigin = [process.env.ORIGIN_1, process.env.ORIGIN_2]
+const devOrigin = ['http://localhost:5173',]
+const allowedOrigins = process.env.NODE_ENV === 'production' ? prodOrigin : devOrigin 
+app.use(cors({
+    origin:(origin, callback)=>{
+        if(!origin || allowedOrigins.includes(origin)) {
+            // console.log(origin, allowedOrigins);
+            callback(null, true);
+        }else {
+            // console.log('Cors origin blocked',origin);
+            callback(new Error('Not Allowed by CORS'));
+        }
+    },
+    credentials:true,
+    methods:['GET','POST','PUT','DELETE'],
+}));
+
 
 app.get("/",(req,res)=>{
     res.send("hello")
